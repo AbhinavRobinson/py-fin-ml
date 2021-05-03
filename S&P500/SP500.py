e0265678923ebbfe2d0b99a6afc3ddba98a61269
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-05-03 13:04:05 +0000

    (feat): check coorelations

--- S&P500/SP500.py
+++ S&P500/SP500.py
@@ -74,7 +74,30 @@ def compile_data():
 
 def visualize_data():
     df = pd.read_csv('sp500_joined_closes.csv')
-    df['AAPL'].plot()
+    df_corr = df.corr()
+    print(df_corr.tail())
+
+    data = df_corr.values
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+    fig.colorbar(heatmap)
+
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+
+    heatmap.set_clim(-1, 1)
+    plt.tight_layout()
     plt.show()
+
 
 visualize_data()
