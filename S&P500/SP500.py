e7711da7e03bda3c2c0a7e875d9744aab73d9afc
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-05-03 12:44:44 +0000

    (feat): add visualize data

--- S&P500/SP500.py
+++ S&P500/SP500.py
@@ -1,10 +1,15 @@
 import bs4 as bs
 import datetime as dt
 import os
+import pandas as pd
 import pandas_datareader.data as web
 import pickle
 import requests
 import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+
+style.use('ggplot')
 
 
 def save_sp500_tickers():
@@ -41,3 +46,35 @@ def get_data_from_yahoo(reload_sp500=False):
             df.to_csv('stock_dfs/{}.csv'.format(ticker))
         else:
             print('Already have {}'.format(ticker))
+
+
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns={'Adj Close': ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closes.csv')
+
+
+def visualize_data():
+    df = pd.read_csv('sp500_joined_closes.csv')
+    df['AAPL'].plot()
+    plt.show()
+
+visualize_data()
